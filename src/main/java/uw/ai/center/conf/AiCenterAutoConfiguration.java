a866ce4bf41190960da9b18ab0e483d59d3f8977
zhangjin <23231269@qq.com>
2025-05-27 22:58:44 +0800

    build: 更新项目版本号
    - 将多个模块的版本号从 2025.0501.1025升级到 2025.0501.1026
    - 移除了多个模块中不必要的 XML 消息转换器配置
    - 调整了日期格式化方式，增加了时区信息
    - 优化了部分代码格式，提高了可读性

--- src/main/java/uw/ai/center/conf/AiCenterAutoConfiguration.java
+++ src/main/java/uw/ai/center/conf/AiCenterAutoConfiguration.java
@@ -2,11 +2,7 @@ package uw.ai.center.conf;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.List;
 
 /**
  * uw-auth-center 自动配置类
@@ -15,14 +11,5 @@ import java.util.List;
 @Configuration
 public class AiCenterAutoConfiguration implements WebMvcConfigurer {
 
-    /**
-     * 移除XML消息转换器
-     *
-     * @param converters
-     */
-    @Override
-    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
-        converters.removeIf( x -> x instanceof MappingJackson2XmlHttpMessageConverter );
-    }
 
 }
